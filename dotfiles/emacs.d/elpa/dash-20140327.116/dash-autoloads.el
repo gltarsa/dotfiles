12dce4897ccc6b961fece921742bc92dbbf51ccc
Greg Tarsa <gltarsa@gmail.com>
2016-03-10 09:51:46 -0500

    Remove Emacs packages from the repo

--- dotfiles/emacs.d/elpa/dash-20140327.116/dash-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21309 51795
-;;;;;;  817428 0))
-
-;;;***
-
-(provide 'dash-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dash-autoloads.el ends here
--- dotfiles/emacs.d/elpa/rtags-20140331.2227/rtags-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rtags" "20140331.2227" "A front-end for rtags" (quote nil))
