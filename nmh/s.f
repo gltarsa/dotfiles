a04950ae65782e3c5414d0ba5d799c73e3ad0a7f
Greg Tarsa <gltarsa@gmail.com>
2016-04-15 16:56:16 -0400

    Update mh_profile and add customization files

---
+++ nmh/s.f
@@ -0,0 +1,5 @@
+%4(msg)%<(cur)+%| %>%<{replied}-%|%<{encrypted}E%| %>%>\
+%<{date}%02(mday{date})-%03(month{date})-%(year{date})%|   *   %>\
+%5(size) \
+%<(mymbox{to})from%14(friendly{from})%|%17(friendly{from})%>  \
+%{subject}
