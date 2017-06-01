deb1a079c26f9665f62b722b81ce792395fa5baf
Greg Tarsa <gltarsa@gmail.com>
2017-06-01 17:11:02 -0400

    Adds script to dump PG db sizes

---
+++ postgres_db_sizes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Display all of the databases with their sizes, largest first.
+psql <<-EOF
+  SELECT d.datname AS Name,  pg_catalog.pg_get_userbyid(d.datdba) AS Owner,
+    CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+        THEN pg_catalog.pg_size_pretty(pg_catalog.pg_database_size(d.datname))
+        ELSE 'No Access'
+    END AS SIZE
+  FROM pg_catalog.pg_database d
+    ORDER BY
+    CASE WHEN pg_catalog.has_database_privilege(d.datname, 'CONNECT')
+        THEN pg_catalog.pg_database_size(d.datname)
+        ELSE NULL
+    END DESC -- nulls first
+    LIMIT 20;
+EOF
+
