86ae448fe363fb8356712699877ec2a585ce5125
Greg Tarsa <gltarsa@gmail.com>
2020-08-06 19:05:20 -0400

    Improvements to set-file-timestamp-* scripts

--- set-file-timestamp-from-exif.sh
+++ set-file-timestamp-from-exif.sh
@@ -1,13 +1,26 @@
-for f in $@
+Usage="
+   $(basename $0) filenames. . .
+
+   This will look at the Exif values for date taken in each file and set the file's
+   modication/create date to that value.
+"
+case $# in
+  0)
+    echo 2>&1 "?Usage ${Usage}"
+    exit 1
+    ;;
+esac
+
+for f in "$@"
 do
-  test -d $f && {
+  test -d "$f" && {
     echo "Skipping: directory '$f'"
     continue
   }
 
-  date=$(exiftool $f | grep 'Date/Time Original' |
+  date=$(exiftool "$f" | grep 'Date/Time Original' |
     sed 's/^.*: 2/2/g;s/ /:/' |
-  awk -F: '{ printf "%s/%s/%s %s:%s:%s\n", $2, $3, $1, $4, $5, $6 }')
+    awk -F: '{ printf "%s/%s/%s %s:%s:%s\n", $2, $3, $1, $4, $5, $6 }')
 
   case $date in
     *:*)
--- set-file-timestamp-from-name.sh
+++ set-file-timestamp-from-name.sh
@@ -1,4 +1,25 @@
-for f in *.jpg
+Usage="
+   $(basename $0) filenames. . .
+
+   This will look at the name of the file for recognized patterns of name/date and attempt
+   to set the file's modication/create date to that value.
+
+   Recognized patterns ("xxxxx" and ".*" = any number of any characters)
+
+   IMG_xxxxx_YYYYMMDD_hhmmss.*
+   IMG-xxxxx_YYYYMMDD_hhmmss.*
+   xxxxIMG_xxxxx_YYYYMMDD_hhmmss.*
+   Resized_xxxxx_YYYYMMDD_hhmmss.*
+   VID_xxxxx_YYYYMMDD_hhmmss.*
+"
+case $# in
+  0)
+    echo 2>&1 "?Usage ${Usage}"
+    exit 1
+    ;;
+esac
+
+for f in "$@"
 do
   # Assumed file name format
   # IMG_20160415_095410983.jpg
@@ -23,8 +44,20 @@ do
       $echo SetFile -m "$date" -d "$date" "$f" && mv "$f" "${f#* }"
       ;;
 
+    *IMG_*)
+      # When std Android name is prefixed: my_picture.IMG_yyyymmdd_hhmmssxxx[_HDR].jpg: set date-time to the second
+      date=$(printf %s "$f" | sed -E 's|^.*IMG_(....)(..)(..)_(..)(..).*|\2/\3/\1 \4:\5|')
+      $echo SetFile -m "$date" -d "$date" "$f" && mv "$f" "${f#* }"
+      ;;
+
+    Resized_*)
+      # Name given by Mark for some files format: Resized_yyyymmdd_hhmmssxxx[_HDR].jpg: set date-time to the second
+      date=$(printf %s "$f" | sed -E 's|^Resized_(....)(..)(..)_(..)(..)(..).*|\2/\3/\1 \4:\5:\6|')
+      $echo SetFile -m "$date" -d "$date" "$f" && mv "$f" "${f#* }"
+      ;;
+
     VID_*)
-      # Standard Android name format: IMG_yyyymmdd_hhmmssxxx[_HDR].jpg: set date-time to the second
+      # Standard Android name format: VID_yyyymmdd_hhmmssxxx[_HDR].jpg: set date-time to the second
       date=$(printf %s "$f" | sed -E 's|^VID_(....)(..)(..)_(..)(..)(..).*|\2/\3/\1 \4:\5:\6|')
       $echo SetFile -m "$date" -d "$date" "$f" && mv "$f" "${f#* }"
       ;;
